c66b5b15c27ecb0090caf11c0dd936efef5a9062
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2020-03-05 19:43:09 -0300

    Adding files

---
+++ precache-manifest.1c256e008ebf94ec606db8c717a35a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69232c240840605f65191b2a6feab124",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4aba8cc38fca7a92fde3",
+    "url": "./static/css/main.d8046bbc.chunk.css"
+  },
+  {
+    "revision": "4d4971dc38174deaa8c4",
+    "url": "./static/js/2.4d4724e7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.4d4724e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aba8cc38fca7a92fde3",
+    "url": "./static/js/main.63d3ee47.chunk.js"
+  },
+  {
+    "revision": "a0d97ef5fba71be525d8",
+    "url": "./static/js/runtime-main.9fbfe5ba.js"
+  }
+]);
